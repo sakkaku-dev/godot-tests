946453c814ed3c9c93f8a1947f9d7a87604d4ff3
myin <minyin142p@gmail.com>
2025-06-09 14:22:56 +0200

    create mines

--- city/timer_label.gd
+++
@@ -1,8 +0,0 @@
-extends Label
-
-@export var timer: Timer
-
-func _process(delta: float) -> void:
-	if timer.is_stopped(): return
-	
-	text = "%.0fs" % timer.time_left
--- simple-space/paper_button.gd
+++
@@ -1,6 +0,0 @@
-extends TextureButton
-
-func _ready() -> void:
-	mouse_entered.connect(func(): modulate = Color.WHITE)
-	mouse_exited.connect(func(): modulate = Color.LIGHT_GRAY)
-	modulate = Color.LIGHT_GRAY
